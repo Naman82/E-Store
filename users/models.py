4c9612b8e9e0633e9f5c8f22980b5f26fffc407a
Naman82 <b520031@iiit-bh.ac.in>
2022-12-22 22:11:25 +0530

    authentication updated

--- users/models.py
+++ users/models.py
@@ -1,27 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
+from email.policy import default
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+import os, uuid
+
+from .managers import UserManager
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
-class AppUser(models.Model):
+
+class User(AbstractUser):
+    def get_update_filename(self, filename):
+        ext = filename.split('.')[-1]
+        filename = "%s.%s" % (uuid.uuid4(), ext)
+        return os.path.join('uploads/user/profile', filename)
+    username = None
     USER_TYPE_CHOICES = [
-        ('S', 'Seller'),
-        ('C', 'Customer'),
+        (0, 'Admin'),
+        (1, 'Professional'),
+        (2, 'Customer'),
     ]
-
-    GENDER_CHOICES = [
-        ('M','Male'),
-        ('F','Female'),
-        ('O','Other'),
-    ]
-
-
-    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="appuser")
-    user_type=models.CharField(max_length=1,choices=USER_TYPE_CHOICES,default='C')
-    gender=models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
-    mobile =models.PositiveIntegerField(null=True, blank=True)
-    address=models.CharField(max_length=500, null=True, blank=True)
-    city=models.CharField(max_length=250, null=True, blank=True)
-    pincode=models.PositiveIntegerField(null=True,blank=True)
+    email = models.EmailField(_('email address'), unique=True, null=True)
+    email_verified = models.BooleanField(default=False)
+    type = models.IntegerField(choices=USER_TYPE_CHOICES, default=2)
+    date_joined = models.DateTimeField(default=timezone.now)
+    phone=models.PositiveBigIntegerField(null=True,blank=True)
+    is_active = models.BooleanField(default=True)
+    profile_pic = models.ImageField(upload_to=get_update_filename, default='uploads/user/profile/default_profile.jpg')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['type']
+    object = UserManager()
 
     def __str__(self):
-        return self.user.username
-    
+        return "{},{},{}".format(self.email, self.first_name, self.type_value)
+
+    def get_user_type(self):
+        return self.type
+
+    @property
+    def type_value(self):
+        return dict(self.USER_TYPE_CHOICES)[self.type]
